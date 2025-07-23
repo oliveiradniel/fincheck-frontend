7e4ceed715ae27db8f326bb2d3b993ecc9953ed3
oliveiradniel <kadadniel@gmail.com>
2025-07-23 11:05:51 -0300

    chore: reorganize the structure of the pages

---
+++ src/view/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+export function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+    </div>
+  );
+}
---
+++ src/view/pages/Register/index.tsx
@@ -0,0 +1,7 @@
+export function Register() {
+  return (
+    <div>
+      <h1>Register Page</h1>
+    </div>
+  );
+}
