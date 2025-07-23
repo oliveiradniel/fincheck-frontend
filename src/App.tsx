3de956a0c024d5a83fa7b84c30d698529d628dde
oliveiradniel <kadadniel@gmail.com>
2025-07-23 10:55:14 -0300

    feat: React Router DOM initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import { Router } from "./Router";
+
 export function App() {
-  return (
-    <div>
-      <h1 className="bg-red-500 text-2xl text-white">Hello World</h1>
-    </div>
-  );
+  return <Router />;
 }
