9b7a86647d80acaae5f47d10acae98121978f362
oliveiradniel <kadadniel@gmail.com>
2025-07-24 13:15:22 -0300

    style: add default cursor when button is disabled

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -7,7 +7,7 @@ export function Button({ className, ...props }: ComponentProps<"button">) {
     <button
       {...props}
       className={clsx(
-        "h-12 cursor-pointer rounded-2xl bg-moss-green px-6 font-medium text-white transition-colors duration-300 ease-in-out hover:bg-moss-green-hover disabled:bg-gray-100 disabled:text-gray-400",
+        "h-12 cursor-pointer rounded-2xl bg-moss-green px-6 font-medium text-white transition-colors duration-300 ease-in-out hover:bg-moss-green-hover disabled:cursor-default disabled:bg-gray-100 disabled:text-gray-400",
         className,
       )}
     />
